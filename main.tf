ddc89196c780aa38df3343499759967fcf953510
Sander van Harmelen <sander@vanharmelen.nl>
2019-07-26 11:59:29 +0200

    remove unneeded quotes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "default" {
+data aws_iam_policy_document default {
   version = "2012-10-17"
   statement {
     effect = "Allow"
@@ -11,37 +11,37 @@ data "aws_iam_policy_document" "default" {
   }
 }
 
-resource "aws_iot_policy" "default" {
+resource aws_iot_policy default {
   name   = "IoTCoreFullAccess-${var.name}"
   policy = data.aws_iam_policy_document.default.json
 }
 
-resource "aws_iot_thing" "default" {
+resource aws_iot_thing default {
   name = var.name
 }
 
-resource "aws_iot_certificate" "default" {
+resource aws_iot_certificate default {
   active = true
 }
 
-resource "aws_iot_policy_attachment" "default" {
+resource aws_iot_policy_attachment default {
   policy = aws_iot_policy.default.name
   target = aws_iot_certificate.default.arn
 }
 
-resource "aws_iot_thing_principal_attachment" "default" {
+resource aws_iot_thing_principal_attachment default {
   principal = aws_iot_certificate.default.arn
   thing     = aws_iot_thing.default.name
 }
 
-module "edgedevice_kms_key" {
+module edgedevice_kms_key {
   source      = "github.com/schubergphilis/terraform-aws-mcaf-kms?ref=v0.1.3"
   name        = var.name
   description = "KMS key used for encrypting SSM Parameters for edge devices"
   tags        = var.tags
 }
 
-resource "aws_ssm_parameter" "certificate_pem" {
+resource aws_ssm_parameter certificate_pem {
   name   = "/${var.name}/iot/certificate-pem"
   type   = "SecureString"
   value  = aws_iot_certificate.default.certificate_pem
@@ -49,7 +49,7 @@ resource "aws_ssm_parameter" "certificate_pem" {
   tags   = var.tags
 }
 
-resource "aws_ssm_parameter" "public_key" {
+resource aws_ssm_parameter public_key {
   name   = "/${var.name}/iot/public-key"
   type   = "SecureString"
   value  = aws_iot_certificate.default.public_key
@@ -57,7 +57,7 @@ resource "aws_ssm_parameter" "public_key" {
   tags   = var.tags
 }
 
-resource "aws_ssm_parameter" "private_key" {
+resource aws_ssm_parameter private_key {
   name   = "/${var.name}/iot/private-key"
   type   = "SecureString"
   value  = aws_iot_certificate.default.private_key
@@ -65,7 +65,7 @@ resource "aws_ssm_parameter" "private_key" {
   tags   = var.tags
 }
 
-data "aws_iam_policy_document" "ssm_activation" {
+data aws_iam_policy_document ssm_activation {
   statement {
     actions = [
       "sts:AssumeRole"
@@ -77,17 +77,17 @@ data "aws_iam_policy_document" "ssm_activation" {
   }
 }
 
-resource "aws_iam_role" "ssm_activation" {
+resource aws_iam_role ssm_activation {
   name               = "SSMActivation-${var.name}"
   assume_role_policy = data.aws_iam_policy_document.ssm_activation.json
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_activation" {
+resource aws_iam_role_policy_attachment ssm_activation {
   role       = "${aws_iam_role.ssm_activation.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
-resource "aws_ssm_activation" "default" {
+resource aws_ssm_activation default {
   name               = var.name
   description        = "SSM Activation for ${var.name}"
   iam_role           = "${aws_iam_role.ssm_activation.id}"
@@ -96,7 +96,7 @@ resource "aws_ssm_activation" "default" {
 }
 
 
-resource "aws_ssm_parameter" "ssm_activation" {
+resource aws_ssm_parameter ssm_activation {
   name   = "/${var.name}/iot/ssm-activation"
   type   = "SecureString"
   value  = aws_ssm_activation.default.activation_code
