71836019c9c79d3e422df65c86a8bfcde2e7e804
Sander van Harmelen <sander@vanharmelen.nl>
2019-08-30 10:23:37 +0200

    Add an optional pathc group variable

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "name" {
   description = "The name of the edge device"
 }
 
+variable "patch_group" {
+  type        = string
+  default     = null
+  description = "Group used by SSM to assign a patch baseline to a device"
+}
+
 variable "tags" {
   type        = map(string)
   description = "A mapping of tags to assign to the SSM Parameter"
