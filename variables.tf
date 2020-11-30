dfe8611358bb69e3cc6dafae1281109c8c56c9b7
Boris Schrijver <bschrijver@schubergphilis.com>
2020-11-30 13:08:25 +0100

    Make SSM Activation role and Thing policy configurable

--- variables.tf
+++ variables.tf
@@ -9,6 +9,18 @@ variable "expiration_duration" {
   description = "The expiration period of the SSM activation, default 4 weeks"
 }
 
+variable "iot_policy" {
+  type        = string
+  default     = null
+  description = "The policy to attach to the Thing"
+}
+
+variable "ssm_activation_role_id" {
+  type        = string
+  default     = null
+  description = "The ID of the role to attach to the SSM activation"
+}
+
 variable "tags" {
   type        = map(string)
   description = "A mapping of tags to assign to the SSM Parameter"
