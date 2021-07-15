3fc8cbdc9e8c7d9f6071aff7905e5bcba4cd38d5
Frank van Boven <frank@cenotaph.nl>
2021-07-15 16:51:14 +0200

    Remove KMS from module and replace with input variable

--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "expiration_duration" {
   description = "The expiration period of the SSM activation, default 4 weeks"
 }
 
+variable "kms_key_id" {
+  type        = string
+  default     = null
+  description = "The KMS key ID used to encrypt all data"
+}
+
 variable "iot_policy" {
   type        = string
   default     = null
